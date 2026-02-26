e30c0943dae00fd2562b29be9d3176d17105cfd2
AbrarShakhi <114344002+AbrarShakhi@users.noreply.github.com>
2026-02-27 01:21:13 +0600

    update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.github.abrarshakhi.noteghty"
         minSdk = 30
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 3
+        versionName = "2.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
